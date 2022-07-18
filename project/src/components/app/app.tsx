1b56655285a69f12ecdf4f84775a9d56336a6ff9
Milkriver <mapav4@yandex.ru>
2022-07-18 15:55:43 +0500

    3.9. Маршрут по тайным тропам

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,10 +1,45 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { AppRoute, AuthorizationStatus } from '../../const';
 import { flats } from '../../mock/flats';
+import Favorites from '../../pages/favorites/favorites';
+import LoginPage from '../../pages/login-page/login-page';
 import MainPage from '../../pages/main-page/main-page';
+import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
+import RoomPage from '../../pages/room-page/room-page';
+import PrivateRoute from '../private-route/private-route';
 
 function App(): JSX.Element {
   return (
-    <div> <MainPage flats={flats}/></div>
-
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<MainPage flats={flats} />}
+        />
+        <Route
+          path={AppRoute.Room}
+          element={<RoomPage />}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={
+            <PrivateRoute
+              authorizationStatus={AuthorizationStatus.NoAuth}
+            >
+              <Favorites />
+            </PrivateRoute>
+          }
+        />
+        < Route
+          path={AppRoute.Login}
+          element={<LoginPage />}
+        />
+        <Route
+          path="*"
+          element={<NotFoundScreen />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- project/src/types/single-flat.ts
+++ project/src/types/single-flat.ts
@@ -8,3 +8,32 @@ export type SingleFlat = {
   bookmark: boolean,
   rating: number,
 };
+
+export type SingleFlatPage = {
+  id: number,
+  name: string,
+  type: string,
+  premium: boolean,
+  image: string[],
+  price: number,
+  bookmark: boolean,
+  rating: number,
+  bedrooms: number,
+  guests: number,
+  options: string[],
+  host: {
+    image: string,
+    name: string,
+    status: string,
+  },
+  description: string,
+  reviews: [
+    {
+      avatar: string,
+      name: string,
+      text: string,
+      date: Date,
+      rating: number,
+    }
+  ],
+};
