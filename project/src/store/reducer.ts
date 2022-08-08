532214e1c201e4dd517cb338a7422c4d1a74b900
Milkriver <mapav4@yandex.ru>
2022-08-08 13:15:27 +0500

    delete: getCityOffersAction

--- project/src/store/reducer.ts
+++ project/src/store/reducer.ts
@@ -1,6 +1,6 @@
 import { createReducer } from '@reduxjs/toolkit';
 import { CITY, offers } from '../mock/offers';
-import { changeCityAction, changeSortOptionAction, getCityOffersAction } from './action';
+import { changeCityAction, changeSortOptionAction } from './action';
 
 const initialState = {
   city: CITY,
@@ -12,9 +12,6 @@ const reducer = createReducer(initialState, (builder) => {
   builder
     .addCase(changeCityAction, (state, action) => {
       state.city = action.payload.city;
-    })
-    .addCase(getCityOffersAction, (state) => {
-      state.offers = offers;
     })
     .addCase(changeSortOptionAction, (state, action) => {
       state.selectedSortOption = action.payload.sortOption;
