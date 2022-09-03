7f1ffe2b09b7664d889f4aff5b4e410864c8ed55
Milkriver <mapav4@yandex.ru>
2022-09-03 14:36:28 +0500

    добавлены тесты

--- project/src/store/user-process/user-process.test.ts
+++ project/src/store/user-process/user-process.test.ts
@@ -13,41 +13,42 @@ describe('Reducer: user', () => {
   });
 
   it('without additional parameters should return initial state', () => {
-    expect(userProcess.reducer(undefined, { type: 'UNKNOWN_ACTION' }))
-      .toEqual({ authorizationStatus: AuthorizationStatus.Unknown, userEmail: '' });
+    expect(userProcess.reducer(undefined, { type: 'UNKNOWN_ACTION' }).authorizationStatus)
+      .toEqual(AuthorizationStatus.Unknown);
   });
 
   describe('checkAuthAction test', () => {
     it('should update authorizationStatus to "AUTH" if checkAuthAction fulfilled', () => {
-      expect(userProcess.reducer(state, { type: checkAuthAction.fulfilled.type }))
-        .toEqual({ authorizationStatus: AuthorizationStatus.Auth, userEmail: '' });
+      expect(userProcess.reducer(state, { type: checkAuthAction.fulfilled.type }).authorizationStatus)
+        .toEqual(AuthorizationStatus.Auth);
     });
     it('should update authorizationStatus to "NO_AUTH" if checkAuthAction rejected', () => {
-      expect(userProcess.reducer(state, { type: checkAuthAction.rejected.type }))
-        .toEqual({ authorizationStatus: AuthorizationStatus.NoAuth, userEmail: '' });
+      expect(userProcess.reducer(state, { type: checkAuthAction.rejected.type }).authorizationStatus)
+        .toEqual(AuthorizationStatus.NoAuth);
     });
   });
 
   describe('loginAction test', () => {
     it('should update authorizationStatus to "AUTH" if loginAction fulfilled', () => {
-      expect(userProcess.reducer(state, { type: loginAction.fulfilled.type }))
-        .toEqual({ authorizationStatus: AuthorizationStatus.Auth, userEmail: '' });
+      expect(userProcess.reducer(state, { type: loginAction.fulfilled.type }).authorizationStatus)
+        .toEqual(AuthorizationStatus.Auth);
     });
     it('should update authorizationStatus to "NO_AUTH" if loginAction rejected', () => {
-      expect(userProcess.reducer(state, { type: loginAction.rejected.type }))
-        .toEqual({ authorizationStatus: AuthorizationStatus.NoAuth, userEmail: '' });
+      expect(userProcess.reducer(state, { type: loginAction.rejected.type }).authorizationStatus)
+        .toEqual(AuthorizationStatus.NoAuth);
     });
   });
 
   describe('logoutAction test', () => {
     it('should update authorizationStatus to "NO_AUTH" if logoutAction fulfilled', () => {
-      expect(userProcess.reducer(state, { type: logoutAction.fulfilled.type }))
-        .toEqual({ authorizationStatus: AuthorizationStatus.NoAuth, userEmail: '' });
+      expect(userProcess.reducer(state, { type: logoutAction.fulfilled.type }).authorizationStatus)
+        .toEqual(AuthorizationStatus.NoAuth);
     });
   });
 
   it('should save user email by a given value', () => {
-    expect(userProcess.reducer(state, saveUserEmail('ma@ya.ru')))
-      .toEqual({ authorizationStatus: AuthorizationStatus.Unknown, userEmail: 'ma@ya.ru' });
+    const email = 'ma@ya.ru';
+    expect(userProcess.reducer(state, saveUserEmail(email)).userEmail)
+      .toEqual(email);
   });
 });
